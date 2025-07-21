24d74e40e2d98fc923c77dec7e53349bdbf78775
pn97arjun <pn97arjun@gmail.com>
2025-07-21 12:58:48 +0530

    mobile devices added

--- playwright.config.ts
+++ playwright.config.ts
@@ -55,10 +55,10 @@ export default defineConfig({
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
     // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+     {
+       name: 'Mobile Safari',
+       use: { ...devices['iPhone 12'] },
+     },
 
     /* Test against branded browsers. */
      {
@@ -68,6 +68,7 @@ export default defineConfig({
      {
        name: 'GoogleChrome',
        use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+       
      },
   ],
 
